7041c53dc0692125b56b54f301411227042ce36f
manyyeon <udayeon@naver.com>
2022-04-02 20:16:59 +0900

    입력 받아서 그 크기만큼 공간 생성

--- src/com/company/MyFrame.java
+++ src/com/company/MyFrame.java
@@ -5,24 +5,37 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
+    Container contentPane;
     SettingDialog settingDialog;
     JPanel menuPanel = new JPanel();
     JPanel titlePanel = new JPanel();
+    JPanel producePanel = new JPanel();
+    JPanel bufferPanel = new JPanel();
+    JPanel consumePanel = new JPanel();
+
     JButton [] menuButton = new JButton[3];
     String [] menuText = {"START", "INITIALIZATION", "SETTING"};
     JLabel [] titleLabel = new JLabel[3];
     String [] titleText = {"Producer", "Bounded Buffer", "Consumer"};
+    int bufferSize = 3; // 버퍼 크기
+    int equationNumber = 10; // 사칙연산 개수
 
     public MyFrame(){
         setTitle("Shared Memory IPC 통신"); // 윈도우 제목 설정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 윈도우를 끄면 프로그램 종료
 
-        Container contentPane = getContentPane(); // 컨텐트팬 가져오기
+        contentPane = getContentPane(); // 컨텐트팬 가져오기
         this.setLocation(400,300); // 윈도우 위치 설정
 
-        // menuPanel, titlePanel 레이아웃 설정
+        // contentPane 레이아웃 설정
+        contentPane.setLayout(new BorderLayout(5,5));
+
+        // panel 레이아웃 설정
         menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.X_AXIS)); // BoxLayout 수평으로
         titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS)); // BoxLayout 수평으로
+        producePanel.setLayout(new BoxLayout(producePanel, BoxLayout.Y_AXIS));
+        bufferPanel.setLayout(new BoxLayout(bufferPanel, BoxLayout.Y_AXIS));
+        consumePanel.setLayout(new BoxLayout(consumePanel, BoxLayout.Y_AXIS));
 
         // menu 컴포넌트들 생성
         for(int i=0; i< menuButton.length; i++){
@@ -52,6 +65,9 @@ public class MyFrame extends JFrame {
         // 컨텐트팬에 패널 부착
         contentPane.add(menuPanel, BorderLayout.SOUTH); // 남쪽에 배치
         contentPane.add(titlePanel, BorderLayout.NORTH); // 북쪽에 배치
+        contentPane.add(new JScrollPane(producePanel), BorderLayout.WEST); // 서쪽에 배치, 스크롤팬에 삽입
+        contentPane.add(new JScrollPane(bufferPanel), BorderLayout.CENTER); // 중앙에 배치, 스크롤팬에 삽입
+        contentPane.add(new JScrollPane(consumePanel), BorderLayout.EAST); // 동쪽에 배치, 스크롤팬에 삽입
 
         // settingDialog 생성
         settingDialog = new SettingDialog(this, "Buffer Size, Equation Number 설정");
@@ -66,28 +82,44 @@ public class MyFrame extends JFrame {
         setVisible(true);
     }
 
+    // 이벤트 리스너
+    // start 리스너
     class StartActionListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
             System.out.println("start");
         }
     }
-
+    // 초기화 리스너
     class InitializationActionListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
             System.out.println("initialization");
         }
     }
-
+    // setting 리스너
     class SettingActionListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
             System.out.println("setting");
             settingDialog.setVisible(true);
 
-            int bufferSize = Integer.parseInt(settingDialog.getInputBoundedBufferSize());
-            int equationNumber = Integer.parseInt(settingDialog.getInputEquationNumber());
+            bufferSize = Integer.parseInt(settingDialog.getInputBoundedBufferSize());
+            equationNumber = Integer.parseInt(settingDialog.getInputEquationNumber());
 
-            System.out.println(bufferSize);
-            System.out.println(equationNumber);
+            // buffer size만큼 공간 만들기
+            for(int i=0; i<bufferSize; i++){
+                JButton buffer = new JButton("(" + (i+1) + ") " + "buffer");
+                buffer.setPreferredSize(new Dimension(300, 200));
+                bufferPanel.add(buffer);
+            }
+            // equation 개수만큼 공간 만들기
+            for(int i=0; i<equationNumber; i++){
+                JButton produce = new JButton("(" + (i+1) + ") " + "produce");
+                JButton consume = new JButton("(" + (i+1) + ") " + "consume");
+                produce.setPreferredSize(new Dimension(300, 200));
+                consume.setPreferredSize(new Dimension(300, 200));
+                producePanel.add(produce);
+                consumePanel.add(consume);
+            }
+            contentPane.revalidate(); // 자식 컴포넌트 다시 배치
         }
     }
 
